4c034882dd2091f8c4fdaa507e9c58be4b793f3c
max <avv123avv@gmail.com>
2018-02-01 10:06:44 +0200

    Add blockchain core

---
+++ src/core/create.js
@@ -0,0 +1,24 @@
+import moment from 'moment';
+import Block from './Block';
+
+const createGenesisBlock = () => (
+  new Block({
+    index: 0,
+    timestamp: moment(),
+    data: 'Genesis Block',
+    previousHash: '0'
+  })
+);
+
+const createNextBlock = ({ previousBlock, data = null }) => (
+  new Block({
+    index: previousBlock.index + 1,
+    data,
+    previousHash: previousBlock.hash
+  })
+);
+
+module.exports = {
+  createGenesisBlock,
+  createNextBlock
+};
